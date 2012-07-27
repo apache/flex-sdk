6ba0db6b10f1ca98bc835ac363ce7b1ae4fa230d
Carol Frampton <cframpton@apache.org>
2012-07-27 20:51:34 +0000

    Script to construct an Apache Flex SDK for an IDE on Windows using a downloaded Apache Flex bin distro (or source distro if you build it) and an Adobe Flex 4.6 SDK.   The script copies the pieces it needs from the Adobe Flex SDK to the Apache Flex SDK.  The easiest place to get the Adobe SDK is from FlashBuilder\sdks\4.6.0.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flex/trunk@1366534 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ide/constructFlexForIDE.bat
@@ -0,0 +1,167 @@
+@echo off
+
+REM ################################################################################
+REM ##
+REM ##  Licensed to the Apache Software Foundation (ASF) under one or more
+REM ##  contributor license agreements.  See the NOTICE file distributed with
+REM ##  this work for additional information regarding copyright ownership.
+REM ##  The ASF licenses this file to You under the Apache License, Version 2.0
+REM ##  (the "License"); you may not use this file except in compliance with
+REM ##  the License.  You may obtain a copy of the License at
+REM ##
+REM ##      http://www.apache.org/licenses/LICENSE-2.0
+REM ##
+REM ##  Unless required by applicable law or agreed to in writing, software
+REM ##  distributed under the License is distributed on an "AS IS" BASIS,
+REM ##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM ##  See the License for the specific language governing permissions and
+REM ##  limitations under the License.
+REM ##
+REM ################################################################################
+
+REM    This script should be used to create an Apache Flex SDK for Microsoft Windows that
+REM    has the directory structure that the an IDE that supports Flex, such as Adobe 
+REM    Flash Builder or JetBrains IntelliJ expects.  If this is a source package, you must
+REM    build the binaries first.  See the README at the root for build instructions.
+REM
+REM    The Adobe AIR SDK for Windows and the Adobe Flash Player playerglobal.swc are 
+REM    copied into the Apache Flex directory.  The paths in the framework 
+REM    configuration files are modified to reflect this.  The AIR_HOME and  
+REM    PLAYERGLOBAL_HOME environment variables are not required because the locations of 
+REM    these pieces are known.
+REM
+REM    TextLayoutFormat (TLF), OSMF, swobject, the Adobe embedded font support, 
+REM    Adobe BlazeDS integration all come from the Adobe Flex 4.6 SDK.
+REM
+REM    The Adobe Flex 4.6 SDK is available here:
+REM         http://www.adobe.com/devnet/flex/flex-sdk-download.html
+REM
+REM    Usage: constructFlexForIDE "Apache Flex dist dir" [Adobe Flex 4.6 dir]
+REM
+
+REM
+REM     Adobe Flex SDK 4.6
+REM
+set ADOBE_FLEX_SDK_DIR=C:/Program Files (x86)/Adobe/Adobe Flash Builder 4.6/sdks/4.6.0
+
+set IDE_SDK_DIR=%~f1
+if not ["%IDE_SDK_DIR%"] == [] goto param2
+echo Usage: %0 "Apache Flex dist dir" [Adobe Flex 4.6 dir]
+goto :eof
+
+:param2
+if not [%2] == [] set ADOBE_FLEX_SDK_DIR=%~f2
+
+echo The Apache Flex for IDE directory is "%IDE_SDK_DIR%"
+echo The Adobe Flex directory is "%ADOBE_FLEX_SDK_DIR%"
+echo.
+
+REM
+REM     If this is an Apache Flex dir then there should be a NOTICE file.
+REM
+if exist "%IDE_SDK_DIR%\NOTICE" goto check2
+echo "%IDE_SDK_DIR%" does not appear to be a Apache Flex distribution.
+goto :eof
+
+REM
+REM     Quick check to see if there are binaries in the Apache distribution.
+REM
+:check2
+if exist "%IDE_SDK_DIR%\lib\mxmlc.jar" goto check3
+echo "%IDE_SDK_DIR%" does not appear to be a Apache Flex distribution with binaries.
+echo If it is a source distribution you must build the binaries first.  See the README.
+goto :eof
+
+REM
+REM     Quick check to see if it is a Flex SDK.
+REM
+:check3
+if exist "%ADOBE_FLEX_SDK_DIR%\license-adobesdk.htm" goto check4
+echo "%ADOBE_FLEX_SDK_DIR%" does not appear to be an Adobe Flex SDK
+goto :eof
+
+:check4
+
+REM
+REM     Copy all the AIR SDK files to the IDE SDK.
+REM     Copy files first, then directories with (/s).
+REM
+echo Copying the AIR SDK files to "%IDE_SDK_DIR%"
+
+for %%G in (
+    AIR SDK license.pdf
+    AIR SDK Readme.txt
+    bin\adl.exe
+    bin\adt.bat
+    lib\adt.jar
+    samples\descriptor-sample.xml) do (
+    
+    if exist "%ADOBE_FLEX_SDK_DIR%"\%%G (
+        copy /y "%ADOBE_FLEX_SDK_DIR%"\%%G "%IDE_SDK_DIR%"\%%G
+    )
+)
+
+for %%G in (
+    frameworks\libs\air
+    frameworks\projects\air
+    include
+    install\android
+    lib\android
+    lib\aot
+    lib\nai
+    lib\win
+    runtimes\air\android
+    runtimes\air\win
+    runtimes\air-captive\win
+    samples\badge
+    samples\icons
+    templates\air
+    templates\extensions) do (
+    
+    if exist "%ADOBE_FLEX_SDK_DIR%"\%%G (
+        REM Make the directory so it won't prompt for file or directory.
+        if not exist "%IDE_SDK_DIR%"\%%G mkdir "%IDE_SDK_DIR%"\%%G
+        xcopy /q /y /e /i /c /r "%ADOBE_FLEX_SDK_DIR%"\%%G "%IDE_SDK_DIR%"\%%G
+        if %errorlevel% NEQ 0 GOTO errorExit
+    )
+)
+
+REM
+REM     Copy all the third-party files from the Adobe Flex SDK to the IDE SDK.
+REM
+echo Copying the third-party files to "%IDE_SDK_DIR%"
+
+for %%G in (
+    frameworks\libs\player\11.1
+    frameworks\javascript\fabridge\samples\fabridge\swfobject) do (
+    
+    if not exist "%IDE_SDK_DIR%"\%%G mkdir "%IDE_SDK_DIR%"\%%G
+    if %errorlevel% NEQ 0 GOTO errorExit
+)
+
+for %%G in (
+    frameworks\libs\osmf.swc
+    frameworks\libs\textLayout.swc
+    frameworks\libs\player\11.1\playerglobal.swc
+    frameworks\javascript\FABridge\samples\fabridge\swfobject\swfobject.js
+    lib\flex-messaging-common.jar
+    lib\afe.jar
+    lib\aglj40.jar
+    lib\flex-fontkit.jar
+    lib\rideau.jar
+    templates\swfobject\swfobject.js) do (
+    
+    copy /y "%ADOBE_FLEX_SDK_DIR%"\%%G "%IDE_SDK_DIR%"\%%G
+)
+
+REM
+REM     Copy config files formatted for IDE to frameworks.
+REM
+echo Copying frameworks config files configured for use without environment variables
+copy /y "%IDE_SDK_DIR%"\ide\flashbuilder\config\*-config.xml "%IDE_SDK_DIR%\frameworks"
+
+goto :eof
+
+:errorExit
+echo Exiting: error %errorlevel%
+exit /b %errorlevel%
