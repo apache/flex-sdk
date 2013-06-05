3e3094e132b220ad005834be62c27c13d1944be8
Om <bigosmallm@gmail.com>
2013-06-05 00:05:25 -0700

    Fix path to mustella mini_run

--- mustella/jenkins.sh
+++ mustella/jenkins.sh
@@ -27,11 +27,9 @@
 export SHELLOPTS
 set -o igncr
 
-cd "C:\ApacheFlex\git\flex-sdk\mustella"
-
-#./mini_run.sh -timeout=60000 tests/components/Label
+./mini_run.sh -timeout=60000 tests/components/Label
 #./mini_run.sh -timeout=60000 tests/gumbo/core/Group/viewport
-./mini_run.sh -timeout=60000 -all
+#./mini_run.sh -timeout=60000 -all
 
 #if [[ -s failures.txt ]] ; then
 #	echo "Some tests failed: running '-failures'" 
