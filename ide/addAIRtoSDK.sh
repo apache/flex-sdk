a0346c9c69bfc4134244262385c28cdcad19c1a7
Justin Mclean <jmclean@apache.org>
2013-07-10 15:33:34 +1000

    removed lab URL for AIR 3.8

--- ide/addAIRtoSDK.sh
+++ ide/addAIRtoSDK.sh
@@ -103,11 +103,6 @@ downloadAIR()
         airDownload="http://airdownload.adobe.com/air/lin/download/${version}/AdobeAIRSDK.tbz2"
     fi
     
-    if [[ ${AIR_VERSION} = "3.8" ]]
-    then
-    	airDownload="http://labsdownload.adobe.com/pub/labs/flashruntimes/air/air3-8_sdk_sa_mac.tbz2"
-    fi
-    
 	echo Downloading AIR ${version}
 	curl ${airDownload} > "${airTempDir}/air.tbz2"
 	
