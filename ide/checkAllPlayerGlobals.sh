882daa4500231eb375f7f470e7db0e65d87ea903
Justin Mclean <jmclean@apache.org>
2013-07-10 15:33:58 +1000

    Added support for Flash Player 11.8 (released version)

--- ide/checkAllPlayerGlobals.sh
+++ ide/checkAllPlayerGlobals.sh
@@ -118,5 +118,5 @@ downloadPlayerGlobal 11.4 e15587856cdb5e21fa1acb6b0610a032 http://download.macro
 downloadPlayerGlobal 11.5 00384b24157442c59ca5d625ecfd11a2 http://download.macromedia.com/get/flashplayer/updaters/11/playerglobal11_5.swc
 downloadPlayerGlobal 11.6 1b841a0a26ada3e5da26eb70c32ab263 http://download.macromedia.com/get/flashplayer/updaters/11/playerglobal11_6.swc
 downloadPlayerGlobal 11.7 12656571c57b2ad641838e5695a00e27 http://download.macromedia.com/get/flashplayer/updaters/11/playerglobal11_7.swc
-downloadPlayerGlobal 11.8 8890d61ce8f96fdb01d2a66290793a8e http://labsdownload.adobe.com/pub/labs/flashruntimes/flashplayer/flashplayer11-8_playerglobal.swc
+downloadPlayerGlobal 11.8 20ce9ae3b2ddd4a5ff3fe65c0a7f1139 http://download.macromedia.com/get/flashplayer/updaters/11/playerglobal11_8.swc
 
