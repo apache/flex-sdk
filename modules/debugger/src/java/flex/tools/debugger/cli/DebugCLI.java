2759e05829c39e6c1b38ffe9d5b11a8ffb30c3cf
Frédéric THMOAS <fthomas@apache.org>
2014-05-03 16:34:31 +0100

    FLEX-34295: info breakpoints should now display the worker ID

--- modules/debugger/src/java/flex/tools/debugger/cli/DebugCLI.java
+++ modules/debugger/src/java/flex/tools/debugger/cli/DebugCLI.java
@@ -1808,6 +1808,12 @@ public class DebugCLI implements Runnable, SourceLocator {
                     sb.append(expr);
             }
 
+            Map<String, Object> workerArgs = new HashMap<String, Object>();
+            workerArgs.put("worker", l.getIsolateId() -1); //$NON-NLS-1$
+            sb.append(" (");
+            sb.append(getLocalizationManager().getLocalizedTextString("inWorker", workerArgs)); //$NON-NLS-1$
+            sb.append(") ");
+
             switch (status) {
                 case BreakAction.UNRESOLVED:
                     sb.append(getLocalizationManager().getLocalizedTextString("breakpointNotYetResolved")); //$NON-NLS-1$
