11fb2bd53f9e609dd9504023694c4367355b8b14
Justin Mclean <jmclean@apache.org>
2016-11-03 10:20:49 +1100

    Added new versions and changed download path from http to https

--- ide/addAIRtoSDK.sh
+++ ide/addAIRtoSDK.sh
@@ -30,7 +30,8 @@
 AIR_VERSION="$1"
 OS=`uname`
 
-if [[ "${AIR_VERSION}" != "20.0"  && "${AIR_VERSION}" != "19.0"  && "${AIR_VERSION}" != "18.0"
+if [[ "${AIR_VERSION}" != "23.0"  && "${AIR_VERSION}" != "22.0"  && "${AIR_VERSION}" != "21.0"
+  &&  "${AIR_VERSION}" != "20.0"  && "${AIR_VERSION}" != "19.0"  && "${AIR_VERSION}" != "18.0"
   && "${AIR_VERSION}" != "17.0" && "${AIR_VERSION}" != "16.0" && "${AIR_VERSION}" != "15.0" 
   && "${AIR_VERSION}" != "14.0" && "${AIR_VERSION}" != "13.0" && "${AIR_VERSION}" != "4.0" 
   && "${AIR_VERSION}" != "3.9" && "${AIR_VERSION}" != "3.8" && "${AIR_VERSION}" != "3.7" 
@@ -38,7 +39,7 @@ if [[ "${AIR_VERSION}" != "20.0"  && "${AIR_VERSION}" != "19.0"  && "${AIR_VERSI
   && "${AIR_VERSION}" != "3.3" && "${AIR_VERSION}" != "3.2" && "${AIR_VERSION}" != "3.1" 
   && "${AIR_VERSION}" != "3.0" && "${AIR_VERSION}" != "2.7" && "${AIR_VERSION}" != "2.6" ]]
 then
-	echo Unknown version ${AIR_VERISON} of AIR. Versions 2.6, 2.7, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0 and 20.0 are supported.
+	echo Unknown version ${AIR_VERISON} of AIR. Versions 2.6, 2.7, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0 and 23.0 are supported.
 	exit 1;
 fi
 
@@ -101,16 +102,10 @@ downloadAIR()
 
     if [[ "${OS}" == "Darwin" ]]
     then
-        airDownload="http://airdownload.adobe.com/air/mac/download/${version}/AdobeAIRSDK.tbz2"
+        airDownload="https://airdownload.adobe.com/air/mac/download/${version}/AdobeAIRSDK.tbz2"
     else
-        airDownload="http://airdownload.adobe.com/air/lin/download/${version}/AdobeAIRSDK.tbz2"
+        airDownload="https://airdownload.adobe.com/air/lin/download/${version}/AdobeAIRSDK.tbz2"
     fi
-    
-    if [ ${AIR_VERSION} = "17.0b" ]
-	then
-		airDownload="http://labsdownload.adobe.com/pub/labs/flashruntimes/air/air17_sdk_sa_mac.tbz2"
-	fi	
-	
 	echo Downloading AIR ${version}
 	curl ${airDownload} > "${airTempDir}/air.tbz2"
 	
@@ -162,6 +157,27 @@ for configFile in "${configFiles[@]}"
 do
 	echo Updating ${configFile}
 	
+	# 23.0 needs FP 23 and swf version 34
+	if [ ${AIR_VERSION} = "23.0" ]
+	then
+		updatePlayerVersion 23.0 "${configFile}"
+		updateSWFVersion 34 "${configFile}"
+	fi
+
+	# 22.0 needs FP 22 and swf version 33
+	if [ ${AIR_VERSION} = "22.0" ]
+	then
+		updatePlayerVersion 22.0 "${configFile}"
+		updateSWFVersion 33 "${configFile}"
+	fi
+
+	# 21.0 needs FP 21 and swf version 32
+	if [ ${AIR_VERSION} = "21.0" ]
+	then
+		updatePlayerVersion 21.0 "${configFile}"
+		updateSWFVersion 32 "${configFile}"
+	fi
+
 	# 20.0 needs FP 20 and swf version 31
 	if [ ${AIR_VERSION} = "20.0" ]
 	then
